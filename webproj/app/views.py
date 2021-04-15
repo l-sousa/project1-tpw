1ee9b234a41102308b5f94fb3ee0de67eed98c82
Francisca Barros <kikas4323@gmail.com>
2021-04-15 16:54:36 +0100

    view work in progress // it aint much but its honest work :(

--- webproj/app/views.py
+++ webproj/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+
+from app.models import *
 
 # Create your views here.
+
+def indexView(request):
+    data = {}
+
+    if request.method == 'GET':
+        categories = []
+        for cat in Category.objects.all():
+            categories.append(cat)
+        data['categories'] = categories
+
+    return render(request, 'index.html', data)
+
--- webproj/webproj/urls.py
+++ webproj/webproj/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.indexView, name='index'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
